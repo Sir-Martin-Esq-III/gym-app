789b280b6082f77301c11480ae032d7c532cf4a2
Thomas <thomasmartin98@hotmail.com>
2021-06-13 21:59:52 +0100

    Switched (nice) to a swtich statement for conditonal rendering

--- src/common/contentDisplay.js
+++ src/common/contentDisplay.js
@@ -6,21 +6,16 @@ import OneRepMaxCalc from './content/ORM/oneRepMaxCalc'
 export default function ContentDisplay(props){
 
     const currentTab=props.tab;
-    {/* maybe make this a switch statement */}
-
-    if (currentTab=="1rm calculator"){
-        return(
-            <div className="content-container">
-            <OneRepMaxCalc/>     
-            </div>
-
-        )
-    }else{
-        return(
-        <div className="content-container">
-            nothing here
-        </div>
-        )
+    
+    switch (currentTab){
+        case "1rm calculator":
+            return(
+                <div className="content-container">
+                    <OneRepMaxCalc/>     
+                </div>
+            )
+        default:
+            return(currentTab)
     }
     
 }
